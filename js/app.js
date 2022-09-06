dde67b6143f3bb391c7b1e3dbb6ffbefda233a03
alenchufacundorios <alenfacurios@gmail.com>
2022-09-06 17:28:31 -0300

    ultimo commit

--- js/app.js
+++ js/app.js
@@ -2,32 +2,45 @@ const inputSearch = document.getElementById("input-search");
 const btnSearch = document.getElementById("btn-search");
 const URL = "https://dragon-ball-super-api.herokuapp.com/api/characters";
 const cards = document.getElementById("cards");
+const load = document.getElementById("load");
 
 //Fetch function
 const getCharacters = async (nameOfCharacter) => {
-  const response = await fetch(URL);
-  const data = await response.json();
+  try{
 
-  for (const key in data) {
-    const element = data[key];
-    const names = element.name.toLowerCase();
-    let positionString = names.indexOf(nameOfCharacter.toLowerCase());
-    if (positionString !== -1) {    
-      cards.innerHTML += `
-      <div class="card">
-      <figure class="card__figure">
-        </figure>
-          <figure class="card__figure--character" style="background-image:url(${element.imageUrl}); background-repeat: no-repeat; background-size: 100% 100%;">
-      </figure>
-      <h1 class="card-title">${element.name}</h1>
-      <div class="card__info">
-        <p class="card__info--specie card__description">Race: ${element.specie}</p>
-        <p class="card__info--status card__description">Status: ${element.status}</p>
-        <p class="card__info--universe card__description">Universe: ${element.universe}</p>
+    const response = await fetch(URL);
+    const data = await response.json();
+  
+    for (const key in data) {
+      const element = data[key];
+      const names = element.name.toLowerCase();
+      let positionString = names.indexOf(nameOfCharacter.toLowerCase());
+      if (positionString !== -1) {
+        setTimeout(() => {
+          cards.innerHTML += `
+          <div class="card">
+          <div class="face front">
+              <img src="${element.imageUrl}" alt="">
+              <h3>${element.name}</h3>
+          </div>
+          <div class="face back">
+              <h3>${element.name}</h3>
+              <br>Raza: ${element.specie}</br>
+              Estado: ${element.status}</br>
+              Universo: ${element.universe}</br>
+              Role: ${element.role}</br>
+              Planeta de origen: ${element.originplanet}</br>
+            </p>
+          </div>
       </div>
-    </div>
-      `;
+          `;
+        }, 3000);
+    
+      }
     }
+  }
+  catch(error){
+    console.log(error)
   }
 };
 
@@ -36,12 +49,20 @@ const cleanHtml = () => {
   cards.innerHTML = "";
 }
 
+// //loading function
+// const loading = () => {
+//   load.innerHTML = 
+//   `
+  
+//   `
+// }
 
 
 //Add function to the button
 btnSearch.addEventListener("click", () => {
-  if (inputSearch != "") {
-    cleanHtml()
+  if (inputSearch.value != "") {
+    cleanHtml();
+    // loading()
     getCharacters(inputSearch.value);
   } else {
     alert("debes ingresar un nombre");
